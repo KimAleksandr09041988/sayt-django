5d2ec311894ff3ae891cfee6847fc89d33201c02
Aleksandr Kim <k1mal3xandre@yandex.ru>
2025-03-28 17:34:52 +0800

    main

--- main/views.py
+++ main/views.py
@@ -4,10 +4,15 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     context = {
-        'title': 'home',
+        'title': 'Home - Главная',
         'content': 'Главная страница магазина - Home'
     }
     return render(request, 'main/index.html', context)
 
 def about(request):
-    return HttpResponse('about page')
+   context = {
+       'title': 'Home - О нас',
+       'content': 'О нас',
+       'text_on_page': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint in laborum eius voluptate rerum doloribus veritatis provident odit nesciunt totam laudantium, saepe deleniti vel! Quo sit architecto deserunt ipsum esse.',
+   }
+   return render(request, 'main/about.html', context)
